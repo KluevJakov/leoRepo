801677397dedbf2b53f58d397b63f0aa70bb3ac8
KluevJakov <kluevja@gmail.com>
2022-02-24 23:50:48 +0400

    1 field and getter/setter and toString

--- src/main/java/com/leojar/collections/DogEnjoyer.java
+++ src/main/java/com/leojar/collections/DogEnjoyer.java
@@ -1,5 +1,20 @@
 package com.leojar.collections;
 
 public class DogEnjoyer {
+    private int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "DogEnjoyer{" +
+                "age=" + age +
+                '}';
+    }
 }
